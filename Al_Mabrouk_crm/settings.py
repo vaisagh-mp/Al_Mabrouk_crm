bc5a515637aa62615c5b9fac81128fe232cf9406
PontusDigimark <contact@pontusdigi.com>
2025-03-01 11:32:05 +0530

    Add new admin

--- Al_Mabrouk_crm/settings.py
+++ Al_Mabrouk_crm/settings.py
@@ -93,23 +93,23 @@ WSGI_APPLICATION = 'Al_Mabrouk_crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'almabroukcrm',
-#             'USER': 'almabroukcrm',
-#             'PASSWORD': 'almabroukcrm',
-#             'HOST': 'localhost',
-#             'PORT': '',
-#         }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'almabroukcrm',
+            'USER': 'almabroukcrm',
+            'PASSWORD': 'almabroukcrm',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+}
 
 
 
